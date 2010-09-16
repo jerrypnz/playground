5c5fdb46ad835fb9ab1390cdfe10bd4c38949430
Jerry Peng <pr2jerry@gmail.com>
2010-09-16 17:52:25 +0800

    change the attribute of the files to 644

--- project-euler/Problem16.c
+++ project-euler/Problem16.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main() {
-
-    
+    long long value = (long long) pow(2.0, 10.0);
+    printf("The result of 2^1000: %lld\n", value);
+    long result = 0;
+    while (value != 0 ) {
+        result += (value % 10);
+        value /= 10;
+    }
+    printf("The result is: %ld\n", result);
 }
