4668ace9547bc20260f5928a454a8c97a6f6d43e
Jerry Peng <jerry@movio.co>
2017-12-30 22:30:49 +1300

    Try latest version of clj

--- clj-projects/core-logic-playground/project.clj
+++ clj-projects/core-logic-playground/project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/moonranger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.logic "0.8.8"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.logic "0.8.11"]])
